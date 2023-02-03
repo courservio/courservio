f9b24263ec58e3813cfa3e21514191938435d4fe
Holger Schmermbeck <hosch@mailbox.org>
2023-02-04 00:24:40 +0100

    small code improvements

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ * Copyright 2023 courservio.de
+ *
+ * Licensed under the EUPL, Version 1.2 or – as soon they
+ * will be approved by the European Commission - subsequent
+ * versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the
+ * Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in
+ * writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied.
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
+ */
+
+declare(strict_types=1);
+
 namespace App\Console\Commands;
 
 use App\Models\Course;
@@ -31,9 +54,9 @@ class GenerateSitemap extends Command
     /**
      * Execute the console command.
      *
-     * @return mixed
+     * @return void
      */
-    public function handle()
+    public function handle(): void
     {
         // check if courses are updated or started yesterday
         $yesterday_courses = Course::wherePublicBookable(1)
