ba9351ad8754b7a449734dbbc1dfc8206b2c9b2c
Holger Schmermbeck <hosch@mailbox.org>
2023-03-03 18:02:08 +0100

    remove friendly captcha

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -81,8 +81,6 @@
                 @error('captcha')
                 <div class="mt-1 flex justify-center text-sm text-red-500">{{ $message }}</div> @enderror
 
-                <livewire:fcaptcha/>
-
                 <div>
                     <button type="submit" wire:loading.attr="disabled"
                             class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
